813d2062dc94cb004e50740999aeee7534f8b2e8
Your Name <baspar.hu@gmail.com>
2019-03-01 17:16:52 +0800

    [edit] Bin - Rofi-Exec on 2 columns now

--- bin/.bin/exec.sh
+++ bin/.bin/exec.sh
@@ -2,6 +2,7 @@
 rofi \
     -show run  \
     -font "MonofurForPowerline Nerd Font 30" \
+    -columns 2 \
     -hide-scrollbar \
     -fullscreen \
     -theme /home/baspar/.config/rofi/baspar.rasi
