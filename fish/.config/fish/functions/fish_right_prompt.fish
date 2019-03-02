7121a3056995f9588abf40865169b16c26c34c1e
Your Name <baspar.hu@gmail.com>
2019-03-03 02:02:04 +0800

    [edit] Fish - Remove date fro mthe right side (Tmux is handling it)

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-    set_color black
-    date +%H:%M:%S | tr -d '\n'
-    set_color normal
+    # set_color black
+    # date +%H:%M:%S | tr -d '\n'
+    # set_color normal
 end
