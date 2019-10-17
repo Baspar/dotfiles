c6a604886cbd3979d60eaec848a3810f57c5acd4
Bastien Laine <me@bastien-laine.io>
2019-10-17 13:24:13 +0800

    [add] Fish: connect-gcloud + port-forward

--- fish/.config/fish/functions/custom_fzf_bindings.fish
+++ fish/.config/fish/functions/custom_fzf_bindings.fish
@@ -1,18 +1,44 @@
 function custom_fzf_bindings
-  function pforward -d "Port forward from k8s"
+  function port-forward -d "Port forward from k8s"
     set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
-    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS $FZF_CTRL_R_OPTS --layout=reverse --header-lines=1 +m"
-    kubectl get services -A \
-        | eval (__fzfcmd --prompt="Select\ a\ service\>\ ")  | sed "s/ \{1,\}/|/g" | cut -d\| -f1,2,6 | read -l service
-      and kubectl get pods -n (echo $service | cut -d\| -f1) \
-        | eval (__fzfcmd  -q (echo $service | cut -d\| -f2) --prompt="Select\ a\ pod\>\ ")  | sed "s/ \{1,\}/|/g" | cut -d\| -f1 | read -l pod
+    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS $FZF_CTRL_R_OPTS --layout=reverse"
 
-    set ports (echo $service | cut -d\| -f3-)
-    commandline -i "$pod|$ports ($service)"
+    kubectl get services -A \
+        | eval (__fzfcmd  --header-lines=1 --prompt="Select\ a\ service\>\ ")  | sed "s/ \{1,\}/|/g" | cut -d\| -f1,2,6 | read -d \| -l namespace service ports
+    [ "$namespace" ] || return
+    [ "$service" ] || return
+    [ "$ports" ] || return
+
+    kubectl get pods -n $namespace \
+        | eval (__fzfcmd  --header-lines=1 -q (echo $service | cut -d\| -f2) --prompt="Select\ a\ pod\>\ ")  | sed "s/ \{1,\}/|/g" | cut -d\| -f1 | read -l pod
+    [ "$pod" ] || return
+
+    set ports_forward (echo $ports | tr , \n | eval (__fzfcmd -m --prompt="Select\ ports\ to\ forward\>\ ") | sed 's#[:/].*##g')
+    [ "$ports_forward" ] || return
+
+    set port_param ""
+    for port in $ports_forward
+      set prompt_cmd "set_color normal; set_color green; echo -n ' Forward $port to > '; set_color normal"
+      while :
+        read -p $prompt_cmd port_forward
+        if [ -z "$port_forward" ]
+          echo "Using $port"
+          set port_forward $port
+          break
+        else if echo $port_forward | grep -q -E '^[0-9]+$'
+          break
+        else
+          echo " Please enter a valid port"
+        end
+      end
+      set port_param "$port_param $port_forward:$port"
+    end
+
+    commandline "kubectl port-forward --namespace $namespace $pod $port_param"
     commandline -f repaint
   end
 
-  function cgc -d "Connect to Gcloud Cluster"
+  function connect-gcloud -d "Connect to Gcloud Cluster"
     set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
     set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS $FZF_CTRL_R_OPTS --layout=reverse --header-lines=1 +m"
     gcloud projects list \
