f9e85ea7e651f434eef96018dcc39af536595981
Baspar <me@bastien-laine.io>
2019-10-30 23:51:56 +0800

    [edit] Fish: use old background instead of next for prompt block

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,46 +1,55 @@
 #!/usr/bin/env fish
 
-# Function block
-#
-# @param BG: Background color of the block
-# @param FG: Foreground color of the block
-# @param NEXT_BG: Background color of subsequent text
-# @param ...TEXT: Text to display in the block
-#
-# @returns: A block with style and text
+function fish_prompt
+  # Function fish_prompt
+  #
+  # @returns A fancy prompt
 
-function block # BG FG NEXT_BG ...TEXT
-    set BG (echo $argv | cut -d\  -f1)
-    set FG (echo $argv | cut -d\  -f2)
-    set NEXT_BG (echo $argv | cut -d\  -f3)
-    set TEXT (echo $argv | cut -d\  -f4-)
+  set -g OLD_BG ""
 
+  function block
+    # Function block
+    #
+    # @param BG: Background color of the block
+    # @param FG: Foreground color of the block
+    # @param ...TEXT: Text to display in the block
+    #
+    # @returns: A block with style and text
+
+    echo $argv | read -d ' ' BG FG TEXT
+    # set BG (echo $argv | cut -d\  -f1)
+    # set FG (echo $argv | cut -d\  -f2)
+    # set TEXT (echo $argv | cut -d\  -f3-)
+
+    if [ "$OLD_BG" != "" ]
+      set_color $OLD_BG -b $BG
+      echo -n ""
+    end
     set_color $FG -b $BG
     echo -n "$TEXT"
-    set_color $BG -b $NEXT_BG
-    echo -n ""
+    set -g OLD_BG $BG
     set_color normal -b normal
-end
+  end
 
-# Function abbr_path
-#
-# @param PATH_PART a part of the PATH
-#
-# @returns: An abbreviated version of the PATH_PART (one letter, but keep prefix special characters)
-#           with $HOME replaced by ~
+  function abbr_path
+    # Function abbr_path
+    #
+    # @param PATH_PART a part of the PATH
+    #
+    # @returns: An abbreviated version of the PATH_PART (one letter, but keep prefix special characters)
+    #           with $HOME replaced by ~
 
-function abbr_path
-  echo -n $argv | sed "s#^$HOME#~#; s#\([^/a-zA-Z0-9]*[a-zA-Z0-9]\)[^/]*/#\1/#g"
-end
+    echo -n $argv | sed "s#^$HOME#~#; s#\([^/a-zA-Z0-9]*[a-zA-Z0-9]\)[^/]*/#\1/#g"
+  end
 
 
-# Function git_block_info
-#
-# @param GIT_ROOT: Absolute path of the git folder
-#
-# @returns: The color and status of the git information at given GIT_ROOT
+  function git_block_info
+    # Function git_block_info
+    #
+    # @param GIT_ROOT: Absolute path of the git folder
+    #
+    # @returns: The color and status of the git information at given GIT_ROOT
 
-function git_block_info
     set GIT_ROOT $argv
 
     set GIT_STATUS (git -C $GIT_ROOT status | grep "^[a-zA-Z0-9]")
@@ -74,51 +83,46 @@ function git_block_info
 
     # Colors and icons
     if [ $GIT_HAS_CHANGES_TO_COMMIT -eq 0 ]
-        set ICONS "$ICONS+"
+      set ICONS "$ICONS+"
     end
     if [ $GIT_HAS_UNSTAGED -eq 0 ]
-        set COLOR "#AF875F"
-        set ICONS "$ICONS~"
+      set COLOR "#AF875F"
+      set ICONS "$ICONS~"
     end
     if [ $GIT_HAS_UNTRACKED -eq 0 ]
-        set COLOR "#AF875F"
-        set ICONS "$ICONS?"
+      set COLOR "#AF875F"
+      set ICONS "$ICONS?"
     end
 
     # Build git string
     echo -n "$COLOR|$GIT_BRANCH $ICONS" | sed 's# $##'
-end
+  end
 
 
-# Function fish_prompt
-#
-# @returns A fancy prompt
+  set_color white -b '#3e3e3e'
 
-function fish_prompt
-    set_color white -b '#3e3e3e'
+  set TOTAL_PATH ''
+  set ACCUMULATED_PATH ''
 
-    set TOTAL_PATH ''
-    set ACCUMULATED_PATH ''
-
-    for PWD_PART in (echo $PWD | sed 's#^/##; s#/$##' |  tr '/' '\n')
-      set ACCUMULATED_PATH "$ACCUMULATED_PATH/$PWD_PART"
-      if [ -e "$TOTAL_PATH$ACCUMULATED_PATH/.git/config" ]
-        git_block_info "$TOTAL_PATH$ACCUMULATED_PATH" | read -d '|' -l GIT_BG_COLOR GIT_STATUS
-        set TOTAL_PATH "$TOTAL_PATH$ACCUMULATED_PATH"
+  for PWD_PART in (echo $PWD | sed 's#^/##; s#/$##' |  tr '/' '\n')
+    set ACCUMULATED_PATH "$ACCUMULATED_PATH/$PWD_PART"
+    if [ -e "$TOTAL_PATH$ACCUMULATED_PATH/.git/config" ]
+      git_block_info "$TOTAL_PATH$ACCUMULATED_PATH" | read -d '|' -l GIT_BG_COLOR GIT_STATUS
+      set TOTAL_PATH "$TOTAL_PATH$ACCUMULATED_PATH"
 
 
-        set ACCUMULATED_PATH (abbr_path "$ACCUMULATED_PATH")
-        block "#3e3e3e" "white" "$GIT_BG_COLOR" " $ACCUMULATED_PATH "
-        block "$GIT_BG_COLOR" "black" "#3e3e3e" " $GIT_STATUS "
-        set ACCUMULATED_PATH ''
-      else if [ -f "$TOTAL_PATH$ACCUMULATED_PATH/.git" ]
-      end
+      set ACCUMULATED_PATH (abbr_path "$ACCUMULATED_PATH")
+      block "#3e3e3e" "white" " $ACCUMULATED_PATH "
+      block "$GIT_BG_COLOR" "black" " $GIT_STATUS "
+      set ACCUMULATED_PATH ''
+    else if [ -f "$TOTAL_PATH$ACCUMULATED_PATH/.git" ]
     end
+  end
 
-    [ "$ACCUMULATED_PATH" ] || set ACCUMULATED_PATH '/'
-    set ACCUMULATED_PATH (abbr_path "$ACCUMULATED_PATH")
+  [ "$ACCUMULATED_PATH" ] || set ACCUMULATED_PATH '/'
+  set ACCUMULATED_PATH (abbr_path "$ACCUMULATED_PATH")
 
-    block "#3e3e3e" "white" "normal" " $ACCUMULATED_PATH "
+  block "#3e3e3e" "white" " $ACCUMULATED_PATH "
 
-    echo -n " "
+  block "normal" "normal" " "
 end
