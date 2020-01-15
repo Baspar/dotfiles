6b307be4f654c7d68a83276b6150c10b6a28f375
Bastien Laine <me@bastien-laine.io>
2020-01-15 12:30:48 +0800

    [edit] Bin: Adapt connect-gcloud to allow any zone

--- bin/.bin/connect-gcloud.sh
+++ bin/.bin/connect-gcloud.sh
@@ -16,13 +16,15 @@ export FZF_DEFAULT_OPTS="--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS $FZF_CTRL_R
 PROJECT=$(gcloud projects list | fzf --prompt="Select a project> "  | sed "s/ \{1,\}/|/g" | cut -d\| -f1)
 [ "$PROJECT" ] || exit 1
 
-CLUSTER=$( \
-  gcloud container clusters list --zone us-central1-a --project "$PROJECT" --format "table(name,location,endpoint,status,currentNodeCount)" \
-  | fzf --prompt="Select a cluster> " | sed "s/ \{1,\}/|/g" | cut -d\| -f1
+CLUSTER_AND_ZONE=$( \
+  gcloud container clusters list --project "$PROJECT" --format "table(name,location,endpoint,status,currentNodeCount)" \
+  | fzf --prompt="Select a cluster> " | sed "s/ \{1,\}/|/g" | cut -d\| -f1,2
 )
+CLUSTER=$(echo "$CLUSTER_AND_ZONE" | cut -d\| -f1)
+ZONE=$(echo "$CLUSTER_AND_ZONE" | cut -d\| -f2)
 [ "$CLUSTER" ] || exit 1
 
-echo "gcloud container clusters get-credentials $CLUSTER --zone us-central1-a --project $PROJECT"
+echo "gcloud container clusters get-credentials $CLUSTER --project $PROJECT --zone $ZONE"
 [ "$MODE" == "normal" ] && {
-  gcloud container clusters get-credentials $CLUSTER --zone us-central1-a --project $PROJECT
+    gcloud container clusters get-credentials $CLUSTER --project $PROJECT --zone $ZONE
 }
