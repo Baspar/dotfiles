2d05d2241cfdd035a7c55fa6e427be61f0f24d43
Bastien Laine <me@bastien-laine.io>
2020-04-29 16:16:37 +0800

    [edit] Tmux: handle C-c in ftsession

--- tmux/.tmux/ftsession.sh
+++ tmux/.tmux/ftsession.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 session=$(tmux list-sessions -F "#{session_name}" | \
-    fzf --query="$1" --select-1 --exit-0) &&
+    fzf --query="$1" --select-1 --exit-0)
+
+if [ $? -ne 0 ]; then exit; fi
 tmux switch-client -t "$session"
