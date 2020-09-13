bd4abccd155a90ea0d27f50beb04959e626f0111
Baspar <me@bastien-laine.io>
2020-09-13 17:02:07 +0800

    [add] Sway: Screenshot util

---
+++ bin/.bin/screenshot.sh
@@ -0,0 +1,49 @@
+#!/bin/env bash
+MODE=$(echo -e "Window\nScreen\nRegion" | rofi \
+    -dmenu \
+    -p "Type of screenshot> " \
+    -i \
+    -matching fuzzy \
+    -fullscreen \
+    -theme "/home/baspar/.config/rofi/baspar.rasi"
+)
+
+case "$MODE" in 
+    Window)
+        GEOMETRY=$(
+            swaymsg -t get_tree \
+                | jq -r ".. \
+                    | select(.pid? and .visible?) \
+                    | .rect \
+                    | \"\(.x),\(.y) \(.width)x\(.height)\"" \
+                | slurp
+            )
+        ;;
+    Screen)
+        GEOMETRY=$(
+            swaymsg -t get_outputs \
+                | jq -r ".[] \
+                    | select(.active) \
+                    | .rect \
+                    | \"\(.x),\(.y) \(.width)x\(.height)\"" \
+                | slurp
+        )
+        ;;
+    Region)
+        GEOMETRY=$(slurp)
+        ;;
+    *)
+        exit
+        ;;
+esac
+
+[ "$GEOMETRY" ] && {
+    if [ "$1" = "--clipboard" ]; then
+        grim -g "$GEOMETRY" -t png - | wl-copy -t image/png
+        notify-send -t 2000 "Screenshot in clipboard"
+    else
+        grim -g "$GEOMETRY" -t png
+        notify-send -t 2000 "Screenshot saved"
+    fi
+
+}
