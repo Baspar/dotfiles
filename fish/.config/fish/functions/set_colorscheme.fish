4ae32b035ca0b6bcae353594a01e0ec110c9deaf
Baspar <me@bastien-laine.io>
2021-11-03 15:43:02 +0100

    [Fish] Add colorscheme script

---
+++ fish/.config/fish/functions/set_colorscheme.fish
@@ -0,0 +1,25 @@
+function set_colorscheme
+  set -U fish_color_autosuggestion 444444
+  set -U fish_color_cancel normal
+  set -U fish_color_command af5f00
+  set -U fish_color_comment 9e9e9e
+  set -U fish_color_cwd 008000
+  set -U fish_color_cwd_root 800000
+  set -U fish_color_end ff5f00
+  set -U fish_color_error ff0000
+  set -U fish_color_escape 00a6b2
+  set -U fish_color_history_current normal
+  set -U fish_color_host normal
+  set -U fish_color_host_remote yellow
+  set -U fish_color_match normal
+  set -U fish_color_normal normal
+  set -U fish_color_operator ffaf5f
+  set -U fish_color_param ffd7af
+  set -U fish_color_quote ffaf5f
+  set -U fish_color_redirection ffd75f
+  set -U fish_color_search_match --background=333
+  set -U fish_color_selection c0c0c0
+  set -U fish_color_status red
+  set -U fish_color_user 00ff00
+  set -U fish_color_valid_path normal
+end
