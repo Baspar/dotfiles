2f8c56a4ab717d576b91639329d65bc4d32d73a3
Baspar <me@bastien-laine.io>
2021-11-11 13:53:13 +0100

    [Fish] Reorganize prompt logic

--- fish/.config/fish/fish_prompt.fish
+++ fish/.config/fish/fish_prompt.fish
@@ -1,64 +1,37 @@
 #!/usr/bin/env fish
 
+# ========
 # Settings
+# ========
+
 set -g FISH_SEPARATOR "$LEFT_SEPARATOR"
 set -g FISH_SUB_SEPARATOR "$LEFT_SUB_SEPARATOR"
 set -g ELLIPSIS "·"
 set -g ELLIPSIS_AFTER "3"
 
+# ========
 # Mappings
-bind -M insert \ce 'set -g __baspar_no_abbr "true"; commandline -f repaint'
-bind -M insert ' ' 'commandline -i " "; commandline -f expand-abbr; emit __baspar_new_word'
+# ========
+
+bind -M insert \ce 'set -g __baspar_no_abbr ""; commandline -f repaint'
+bind -M insert ' ' 'commandline -i " "; commandline -f expand-abbr; emit __baspar_check_special_command'
 bind -M insert \c] 'emit __baspar_cycle_indicator'
 
+# ==================
 # Internal variables
-set -g __baspar_no_abbr ''
+# ==================
+
 set -g __baspar_old_bg ""
 set -g __baspar_fish_promp_count 0
 set -g __baspar_fish_last_promp_count 0
 
-function __baspar_cycle_indicator --on-event __baspar_cycle_indicator
-  if set -q AWS_PROFILE && ! set -q __baspar_hide_aws
-    set AWS_PROFILES (cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/')
-    set count (count $AWS_PROFILES)
-    set -g __baspar_aws_id (math $__baspar_aws_id % $count + 1)
-    set -gx AWS_PROFILE $AWS_PROFILES[$__baspar_aws_id]
-    commandline -f repaint
-  end
-end
-
-function __baspar_aws_indicator_fn --on-event __baspar_aws_indicator
-  set AWS_PROFILES ([ -f ~/.aws/credentials ] && cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/'); or return
-
-  if ! set -q AWS_PROFILE
-    set -g __baspar_aws_id 1
-    set -gx AWS_PROFILE $AWS_PROFILES[$__baspar_aws_id]
-  end
-
-  set -e __baspar_hide_aws
-  commandline -f repaint
-end
-
-function fish_right_prompt
-  set -g __baspar_old_bg "black"
-
-  if set -q AWS_PROFILE && ! set -q __baspar_hide_aws
-    set AWS_PROFILES (cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/')
-    set count (count $AWS_PROFILES)
-    block "#AF875F" "#000000" " $AWS_PROFILE" -o -i
-    block (__baspar_darker_of "#AF875F") "#3e3e3e" "$__baspar_aws_id/$count" -o
-  end
-end
-
-function __baspar_listen_to_special_command --on-event __baspar_new_word
-  switch (commandline | cut -d' ' -f1)
-    case "aws"
-      emit __baspar_aws_indicator
-  end
-end
+# =======
+# Helpers
+# =======
 
 function block -a BG FG TEXT
   # Same as _block, but wrap text with spaces
+  #
   set FLAGS "$argv[4..-1]"
 
   _block "$BG" "$FG" " $TEXT " $FLAGS
@@ -73,7 +46,7 @@ function _block -a BG FG TEXT
   # @param ...FLAGS: 0+ Flags for set_color
   #
   # @returns: A block with style and text
-
+  #
   set FLAGS "$argv[4..-1]"
 
   [ -z $FLAGS ] || eval set_color $FLAGS
@@ -105,15 +78,64 @@ function _block -a BG FG TEXT
   set_color normal -b normal
 end
 
-function fish_mode_prompt
-  # Disable default Vi prompt
+function __baspar_darker_of -a COLOR
+  # Function __baspar_darker_of
+  #
+  # @param COLOR a color part of the list
+  #
+  # @returns: A darker shade of this color
+  #
+  if [ "$COLOR" = "#AF5F5E" ]
+    echo -n "#703D3D"
+  else if [ "$COLOR" = "#888888" ]
+    echo -n "#555555"
+  else if [ "$COLOR" = "#AF875F" ]
+    echo -n "#926E49"
+  else if [ "$COLOR" = "#4B8252" ]
+    echo -n "#38623E"
+  else
+    echo -n ""
+  end
+end
+
+# =============
+# AWS indicator
+# =============
+
+function __baspar_cycle_indicator --on-event __baspar_cycle_indicator
+  if set -q AWS_PROFILE && ! set -q __baspar_hide_aws
+    set AWS_PROFILES (cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/')
+    set count (count $AWS_PROFILES)
+    set -g __baspar_aws_id (math $__baspar_aws_id % $count + 1)
+    set -gx AWS_PROFILE $AWS_PROFILES[$__baspar_aws_id]
+    commandline -f repaint
+  end
+end
+
+function __baspar_aws_indicator_fn --on-event __baspar_aws_indicator
+  set AWS_PROFILES ([ -f ~/.aws/credentials ] && cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/'); or return
+
+  if ! set -q AWS_PROFILE
+    set -g __baspar_aws_id 1
+    set -gx AWS_PROFILE $AWS_PROFILES[$__baspar_aws_id]
+  end
+
+  set -e __baspar_hide_aws
+  commandline -f repaint
+end
+
+function __baspar_check_special_command_fn --on-event __baspar_check_special_command
+  switch (commandline | cut -d' ' -f1)
+    case "aws"
+      emit __baspar_aws_indicator
+  end
 end
 
 function fish_custom_mode_prompt
   # Function fish_mode_prompt
   #
   # @returns Vi mode prompt
-
+  #
   set LETTER ""
 
   # In SSH
@@ -143,41 +165,24 @@ function fish_custom_mode_prompt
   _block "$BG_COLOR" "#000000" " $LETTER "
 end
 
-function __baspar_abbr_path -a PATH_PART
+# =================
+# Main prompt logic
+# =================
+
+function __baspar_abbr_path -a PATH_SEGMENT
   # Function __baspar_abbr_path
   #
-  # @param PATH_PART a part of the PATH
+  # @param PATH_SEGMENT a part of the PATH
   #
-  # @returns: An abbreviated version of the PATH_PART (one letter, but keep prefix special characters)
+  # @returns: An abbreviated version of the PATH_SEGMENT (one letter, but keep prefix special characters)
   #           with $HOME replaced by ~
-
-  if [ -z $__baspar_no_abbr ]
-    echo -n "$PATH_PART" | sed "s#^$HOME#~#; s#\([^/]\{$ELLIPSIS_AFTER\}\)[^/]\{1,\}/#\1$ELLIPSIS/#g"
+  #
+  if set -q $__baspar_no_abbr
+    echo -n "$PATH_SEGMENT" | sed "s#^$HOME#~#; s#\([^/]\{$ELLIPSIS_AFTER\}\)[^/]\{1,\}/#\1$ELLIPSIS/#g"
   else
-    echo -n "$PATH_PART" | sed "s#^$HOME#~#"
+    echo -n "$PATH_SEGMENT" | sed "s#^$HOME#~#"
   end
 end
-
-function __baspar_darker_of -a COLOR
-  # Function __baspar_darker_of
-  #
-  # @param COLOR a color part of the list
-  #
-  # @returns: A darker shade of this color
-
-  if [ "$COLOR" = "#AF5F5E" ]
-    echo -n "#703D3D"
-  else if [ "$COLOR" = "#888888" ]
-    echo -n "#555555"
-  else if [ "$COLOR" = "#AF875F" ]
-    echo -n "#926E49"
-  else if [ "$COLOR" = "#4B8252" ]
-    echo -n "#38623E"
-  else
-    echo -n ""
-  end
-end
-
 
 function __baspar_git_branch_name -a GIT_DIR GIT_WORKTREE
   # Function __baspar_git_branch_name
@@ -185,7 +190,7 @@ function __baspar_git_branch_name -a GIT_DIR GIT_WORKTREE
   # @param GIT_DIR location of .git folder
   #
   # @returns:
-
+  #
   [ -d "$GIT_WORKTREE/rebase-merge" ] && {
     cat "$GIT_WORKTREE/rebase-merge/head-name" 2>/dev/null
     return
@@ -208,7 +213,7 @@ function __baspar_git_operation -a GIT_DIR GIT_WORKTREE
   # @param GIT_DIR location of .git folder
   #
   # @returns: a symbol corresponding to the current operation
-
+  #
   if test -d "$GIT_DIR/rebase-merge"
       set step (cat "$GIT_DIR/rebase-merge/msgnum" 2>/dev/null)
       set total (cat "$GIT_DIR/rebase-merge/end" 2>/dev/null)
@@ -240,7 +245,7 @@ function __baspar_async_git_status -a GIT_DIR GIT_WORKTREE
   # @param GIT_DIR location of .git folder
   #
   # @returns:
-
+  #
   set -l changedFiles (command git -C "$GIT_WORKTREE" diff --name-status 2>/dev/null | string match -r \\w)
   set -l stagedFiles (command git -C "$GIT_WORKTREE" diff --staged --name-status | string match -r \\w)
 
@@ -264,7 +269,7 @@ function __baspar_git_ahead_behind -a GIT_DIR GIT_WORKTREE
   # @param GIT_DIR location of .git folder
   #
   # @returns:
-
+  #
   set GIT_AHEAD 0
   set GIT_BEHIND 0
   set GIT_UPSTREAM (command git -C "$GIT_WORKTREE" rev-parse --abbrev-ref --symbolic-full-name @{u} 2> /dev/null)
@@ -289,7 +294,7 @@ function __baspar_git_block_info -a GIT_DIR GIT_WORKTREE NEED_GIT_STATUS_UPDATE
   # @param GIT_DIR: Absolute path of the .git folder
   #
   # @returns: The color and status of the git information at given GIT_DIR
-
+  #
   set SAFE_GIT_DIR (string escape --style=var "$GIT_DIR")
 
   __baspar_git_branch_name "$GIT_DIR" "$GIT_WORKTREE"  | read -l GIT_BRANCH
@@ -354,12 +359,22 @@ function __baspar_git_block_info -a GIT_DIR GIT_WORKTREE NEED_GIT_STATUS_UPDATE
 end
 
 function __baspar_cleanup --on-event fish_cancel
+  # Function __baspar_cleanup
+  #
+  # Clean all indicators when C-c
+  #
   set -g __baspar_hide_aws 'true'
 end
 
 function __baspar_set_fish_promp_count --on-event fish_prompt
+  # Function __baspar_set_fish_promp_count
+  #
+  # Increment a counter for every "fresh" repaint and
+  # cleanup some variable
+  #
   set -g __baspar_fish_promp_count (math $__baspar_fish_promp_count + 1)
   set -g __baspar_hide_aws 'true'
+  set -e __baspar_no_abbr
 end
 
 function __baspar_get_git_dir -a GIT_WORKTREE
@@ -368,7 +383,7 @@ function __baspar_get_git_dir -a GIT_WORKTREE
   # @param GIT_WORKTREE: root path of git worktree
   #
   # @returns: Actual location of the git config folder
-
+  #
   if [ -f "$GIT_WORKTREE/.git" ]
     set GIT_DIR (cat "$GIT_WORKTREE/.git" | grep "^gitdir" | sed 's#^gitdir: *##')
     if ! string match -r "^/" "$GIT_DIR" &> /dev/null
@@ -381,11 +396,51 @@ function __baspar_get_git_dir -a GIT_WORKTREE
   echo $GIT_DIR
 end
 
+function __baspar_update_path_segments_abbr --on-variable __baspar_no_abbr
+  # Function __baspar_update_path_segments_abbr
+  #
+  # Cache list of abbreviated path segments
+  #
+  set -e __baspar_path_segments_abbr
+  for PATH_SEGMENT in $__baspar_path_segments
+    set -gx --append __baspar_path_segments_abbr (__baspar_abbr_path "$PATH_SEGMENT")
+  end
+
+  commandline -f repaint
+end
+
+function __baspar_update_path_segments --on-variable PWD
+  # Function __baspar_update_path_segments
+  #
+  # Cache list of path segments
+  #
+  set -e __baspar_path_segments
+
+  for PATH_SEGMENT in (echo $PWD | sed 's#^/##; s#/$##' | tr '/' '\n')
+    set ACCUMULATED_PATH "$ACCUMULATED_PATH/$PATH_SEGMENT"
+    if [ -e "$TOTAL_PATH$ACCUMULATED_PATH/.git" ]
+      set -gx --append __baspar_path_segments "$ACCUMULATED_PATH"
+      set TOTAL_PATH "$TOTAL_PATH$ACCUMULATED_PATH"
+      set ACCUMULATED_PATH ''
+    end
+  end
+
+  [ -z "$ACCUMULATED_PATH" ] && set ACCUMULATED_PATH '/'
+
+  set -g --append __baspar_path_segments "$ACCUMULATED_PATH"
+
+  __baspar_update_path_segments_abbr
+end
+
+# =======
+# Prompts
+# =======
+
 function fish_prompt
   # Function fish_prompt
   #
   # @returns Main prompt
-
+  #
   set -l _display_status $status
 
   set -g __baspar_old_bg ""
@@ -401,46 +456,41 @@ function fish_prompt
     block "#4B8252" "#3e3e3e" "$VENV_NAME" -i -o
   end
 
-  set TOTAL_PATH ''
-  set ACCUMULATED_PATH ''
-
   set NEED_GIT_STATUS_UPDATE ""
   if [ $__baspar_fish_promp_count -ne $__baspar_fish_last_promp_count ]
     set -g __baspar_fish_last_promp_count $__baspar_fish_promp_count
     set NEED_GIT_STATUS_UPDATE "true"
   end
 
-  for PWD_PART in (echo $PWD | sed 's#^/##; s#/$##' |  tr '/' '\n')
-    set ACCUMULATED_PATH "$ACCUMULATED_PATH/$PWD_PART"
-    if [ -e "$TOTAL_PATH$ACCUMULATED_PATH/.git" ]
-      set GIT_WORKTREE "$TOTAL_PATH$ACCUMULATED_PATH"
-      set GIT_DIR (__baspar_get_git_dir "$GIT_WORKTREE")
+  # Initialize path segment
+  set -q __baspar_path_segments || __baspar_update_path_segments
 
-      __baspar_git_block_info "$GIT_DIR" "$GIT_WORKTREE" "$NEED_GIT_STATUS_UPDATE" | read -d '|' GIT_BG_COLOR GIT_BRANCH GIT_OPERATION GIT_ICONS
+  set TOTAL_PATH ''
+  for i in (seq (count $__baspar_path_segments))
+    set PATH_SEGMENT_ABBR $__baspar_path_segments_abbr[$i]
+    set TOTAL_PATH $TOTAL_PATH$__baspar_path_segments[$i]
 
-      set TOTAL_PATH "$TOTAL_PATH$ACCUMULATED_PATH"
+    # Path part block
+    block "#3e3e3e" "#FFFFFF" "$PATH_SEGMENT_ABBR"
 
-      set ACCUMULATED_PATH (__baspar_abbr_path "$ACCUMULATED_PATH")
-      block "#3e3e3e" "#FFFFFF" "$ACCUMULATED_PATH"
+    # No git block for last part
+    [ $i -eq (count $__baspar_path_segments) ] && break
 
-      set GIT_FG_COLOR "#3e3e3e"
-      set SAFE_GIT_DIR (string escape --style=var "$GIT_DIR")
-      if set -q __baspar_git_status_pid_$SAFE_GIT_DIR
-        set GIT_FG_COLOR "#666666"
-      end
+    # Git block
+    set GIT_WORKTREE "$TOTAL_PATH"
+    set GIT_DIR (__baspar_get_git_dir "$GIT_WORKTREE")
+    __baspar_git_block_info "$GIT_DIR" "$GIT_WORKTREE" "$NEED_GIT_STATUS_UPDATE" | read -d '|' GIT_BG_COLOR GIT_BRANCH GIT_OPERATION GIT_ICONS
 
-      [ -n "$GIT_OPERATION" ] && block (__baspar_darker_of $GIT_BG_COLOR)  "#3e3e3e" "$GIT_OPERATION" -o -i
-      block "$GIT_BG_COLOR" $GIT_FG_COLOR "$GIT_BRANCH" -o -i
-      [ -n "$GIT_ICONS" ] && block (__baspar_darker_of $GIT_BG_COLOR) "#3e3e3e" "$GIT_ICONS" -o -i
-
-      set ACCUMULATED_PATH ''
+    set GIT_FG_COLOR "#3e3e3e"
+    set SAFE_GIT_DIR (string escape --style=var "$GIT_DIR")
+    if set -q __baspar_git_status_pid_$SAFE_GIT_DIR
+      set GIT_FG_COLOR "#666666"
     end
+
+    [ -n "$GIT_OPERATION" ] && block (__baspar_darker_of $GIT_BG_COLOR) "#3e3e3e" "$GIT_OPERATION" -o -i
+    block "$GIT_BG_COLOR" $GIT_FG_COLOR "$GIT_BRANCH" -o -i
+    [ -n "$GIT_ICONS" ] && block (__baspar_darker_of $GIT_BG_COLOR) "#3e3e3e" "$GIT_ICONS" -o -i
   end
-
-  [ "$ACCUMULATED_PATH" ] || set ACCUMULATED_PATH '/'
-  set ACCUMULATED_PATH (__baspar_abbr_path "$ACCUMULATED_PATH")
-
-  block "#3e3e3e" "#FFFFFF" "$ACCUMULATED_PATH"
 
   _block "normal" "normal" ""
   echo ""
@@ -449,8 +499,19 @@ function fish_prompt
   fish_custom_mode_prompt
   _block "normal" "normal" ""
   echo " "
+end
 
-  if ! [ -z __baspar_no_abbr ]
-    set -g __baspar_no_abbr ''
+function fish_right_prompt
+  set -g __baspar_old_bg "black"
+
+  if set -q AWS_PROFILE && ! set -q __baspar_hide_aws
+    set AWS_PROFILES (cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/')
+    set count (count $AWS_PROFILES)
+    block "#AF875F" "#000000" " $AWS_PROFILE" -o -i
+    block (__baspar_darker_of "#AF875F") "#3e3e3e" "$__baspar_aws_id/$count" -o
   end
+end
+
+function fish_mode_prompt
+  # Disable default Vi prompt
 end
