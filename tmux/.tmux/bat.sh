806137d37b25722509cc2d26c7832605d89be0ba
Baspar <me@bastien-laine.io>
2022-03-17 21:28:00 +0000

    [tmux] Change bat.sh script

--- tmux/.tmux/bat.sh
+++ tmux/.tmux/bat.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-if [ $(command -v acpi) ]
-then
+if [ $(command -v acpi) ]; then
     INFO=$(acpi)
     PERCENTAGE=$(echo "$INFO" | grep -o "\([0-9]\+\)%" | sed 's/%//')
     DISCHARGING=$(echo "$INFO" | grep -i "discharging")
     CHARGING=$(echo "$INFO" | grep -i "charging")
-elif [ $(command -v pmset) ]
-then
+elif [ $(command -v pmset) ]; then
     INFO=$(pmset -g batt)
     PERCENTAGE=$(echo "$INFO" | grep -o "\([0-9]\+\)%" | sed 's/%//')
     DISCHARGING=$(echo "$INFO" | grep -i "discharging")
@@ -14,32 +12,25 @@ then
     CHARGED=$(echo "$INFO" | grep -i "charged")
 fi
 
-[ "$PERCENTAGE" ] || {
-    return ""
-}
-
-if [ "$DISCHARGING" ]
-then
-    if [ $PERCENTAGE -lt 10 ]
-    then
+if [ "$DISCHARGING" ]; then
+    if [ $PERCENTAGE -lt 10 ]; then
         COLOR='#AF5F5E'
         FONT='#3e3e3e'
-    elif [ $PERCENTAGE -lt 25 ]
-    then
+    elif [ $PERCENTAGE -lt 25 ]; then
         COLOR='#af875f'
         FONT='#3e3e3e'
     else
         COLOR='#4e4e4e'
         FONT='white'
     fi
-elif [ "$CHARGING" ] ||[ "$CHARGED" ]
-then
+elif [ "$CHARGING" ] ||[ "$CHARGED" ]; then
     COLOR='#4e4e4e'
     FONT='white'
 else
     COLOR='#AF5F5E'
-    FONT='white'
+    FONT='black'
 fi
+
 declare -A ICONS
 ICONS["0"]=""
 ICONS["10"]=""
@@ -53,5 +44,10 @@ ICONS["80"]=""
 ICONS["90"]=""
 ICONS["100"]=""
 
-ICON=${ICONS[$(echo "$PERCENTAGE/10*10" | bc)]}
-echo "#[fg=$COLOR]$RIGHT_SEPARATOR#[bg=$COLOR,fg=$FONT] $ICON$PERCENTAGE% "
+if [ -n "$PERCENTAGE" ]; then
+    INFO="${ICONS[$(( PERCENTAGE / 10 * 10 ))]}$PERCENTAGE%"
+else
+    INFO=""
+fi
+
+echo "#[fg=$COLOR]$RIGHT_SEPARATOR#[bg=$COLOR,fg=$FONT] $INFO "
