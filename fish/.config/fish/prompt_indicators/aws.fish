be1832dac2aeed4062db534a8f0ca8bc6afe805b
Baspar <me@bastien-laine.io>
2022-05-05 10:18:00 +0200

    [Fish] Simplify AWS Indicator

--- fish/.config/fish/prompt_indicators/aws.fish
+++ fish/.config/fish/prompt_indicators/aws.fish
@@ -3,7 +3,7 @@
 # ================
 
 function __baspar_indicator_list_aws
-  [ -f ~/.aws/credentials ] && cat ~/.aws/credentials | sed '/^\[.*\]$/!d; s/\[\(.*\)\]/\1/'
+  aws configure list-profiles
 end
 
 function __baspar_indicator_pre_async_aws -a item
@@ -11,9 +11,11 @@ function __baspar_indicator_pre_async_aws -a item
 end
 
 function __baspar_indicator_async_aws -a AWS_PROFILE response_file
-  set AWS_REGION (env AWS_PROFILE=$AWS_PROFILE aws configure get region)
-  set AWS_ACCOUNT (env AWS_PROFILE=$AWS_PROFILE aws sts get-caller-identity --query Account --output text); or return 2
-  set AWS_DEV (env AWS_PROFILE=$AWS_PROFILE aws configure get dev)
+  set -fx AWS_PROFILE $AWS_PROFILE
+
+  set AWS_REGION (aws configure get region)
+  set AWS_ACCOUNT (aws sts get-caller-identity --query Account --output text); or return 2
+  set AWS_DEV (aws configure get dev)
 
   echo "$AWS_REGION" > $response_file
   echo "$AWS_ACCOUNT" >> $response_file
