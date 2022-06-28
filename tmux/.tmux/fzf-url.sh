646db091ce2098d5b72018f7498144cf67fdb567
Baspar <me@bastien-laine.io>
2022-06-28 18:06:18 +0200

    [Tmux] Capture visible part of pane to fzf-url when in copy mode

--- tmux/.tmux/fzf-url.sh
+++ tmux/.tmux/fzf-url.sh
@@ -10,7 +10,14 @@ open_url() {
     fi
 }
 
-content="$(tmux capture-pane -J -p)"
+current_pane=$(tmux list-panes -F "#{pane_active}|#{scroll_position}|#{pane_height}" | grep "^1|")
+scroll_position=$(echo $current_pane | cut -d\| -f2)
+pane_height=$(echo $current_pane | cut -d\| -f3)
+if [ -z $scroll_position ]; then
+    content="$(tmux capture-pane -J -p)"
+else
+    content="$(tmux capture-pane -J -p -S -$scroll_position -E $(( $pane_height - $scroll_position - 1)))"
+fi
 
 urls=$(echo "$content" | grep -oE '(https?|ftp|file):/?//[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]')
 wwws=$(echo "$content" | grep -oE '(http?s://)?www\.[a-zA-Z](-?[a-zA-Z0-9])+\.[a-zA-Z]{2,}(/\S+)*' | grep -vE '^https?://' | sed 's/^\(.*\)$/http:\/\/\1/')
@@ -22,7 +29,9 @@ items=$(printf '%s\n' "$urls" "$wwws" "$ips" "$gits" "$extras" |
     sort -u
 )
 
-[ -z "$items" ] && tmux display 'tmux-fzf-url: no URLs found' && exit
+[ -z "$items" ] && tmux display 'tmux-fzf-url: no URLs found' && {
+    exit
+}
 
 item=$(echo "$items" | fzf --no-preview --no-border)
 
