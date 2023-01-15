8fa2a8ed72ac9e8a6c1f2f0053a25ac8672d90d5
Baspar <me@bastien-laine.io>
2023-01-15 16:01:06 +0100

    [Fish] Add `get_or_set_aws_config`

--- fish/.config/fish/conf.d/aws2.fish
+++ fish/.config/fish/conf.d/aws2.fish
@@ -10,3 +10,17 @@
 #     | jq -r '[.AccessKeyId,.SecretAccessKey,.SessionToken] | join(" ")' \
 #     | read -gx AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
 # end
+
+function get_or_set_aws_config -a key -a cmd
+  set config_file "$HOME/.mde_aws_config.json"
+  [ -f "$config_file" ] || echo "{}" > "$config_file"
+
+  set out (jq -r --exit-status ".[\"$AWS_PROFILE\"][\"$key\"]" "$config_file")
+  if [ $status -ne 0 ]
+    set out (eval $cmd); or return 1
+    set config (cat "$config_file")
+    echo "$config" | jq ".[\"$AWS_PROFILE\"][\"$key\"] = \"$out\"" > "$config_file"
+  end
+
+  echo $out
+end
