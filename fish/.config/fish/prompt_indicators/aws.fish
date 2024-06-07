bcbbf445478ff14bfd5ccb68bf736c459a245cc6
Baspar <me@bastien-laine.io>
2024-06-07 12:59:05 +0200

    [Fish] Add Role to AWS indicator

--- fish/.config/fish/prompt_indicators/aws.fish
+++ fish/.config/fish/prompt_indicators/aws.fish
@@ -29,7 +29,9 @@ function __baspar_indicator_async_aws -a credentials response_file
   set AWS_REGION (aws configure get region; or echo "eu-west-1")
   set AWS_DEV (aws configure get dev)
   if [ -n "$is_overriden" ]
-    set AWS_ACCOUNT (aws sts get-caller-identity --query Account --output text); or exit 2
+    set account_info (aws sts get-caller-identity); or exit 2
+    set AWS_ACCOUNT (echo "$account_info" | jq -r ".Account")
+    set AWS_ROLE (echo "$account_info" | jq -r '.Arn')
   else
     set AWS_ACCOUNT (get_or_set_aws_config aws_account_id "aws sts get-caller-identity --query Account --output text"); or exit 2
   end
@@ -38,6 +40,7 @@ function __baspar_indicator_async_aws -a credentials response_file
   echo "$AWS_REGION" >> $response_file
   echo "$AWS_ACCOUNT" >> $response_file
   echo "$AWS_DEV" >> $response_file
+  echo "$AWS_ROLE" >> $response_file
 end
 
 function __baspar_indicator_async_cb_aws -a response_file
@@ -47,6 +50,7 @@ function __baspar_indicator_async_cb_aws -a response_file
   set -gx AWS_REGION $async_response[2]
   set -gx AWS_ACCOUNT_ID $async_response[3]
   set AWS_DEV $async_response[4]
+  set AWS_ROLE $async_response[5]
 
   if [ -n "$AWS_DEV" ]
     set -gx DEV_AWS_ACCOUNT $AWS_ACCOUNT_ID
@@ -54,8 +58,9 @@ function __baspar_indicator_async_cb_aws -a response_file
     set -e DEV_AWS_ACCOUNT
   end
 
+    set AWS_PRETTY_ROLE ( echo $AWS_ROLE | sed 's#^.*:assumed-role/\(.*\)/[^/]*$#\1#g')
   if [ -n "$is_overriden" ]
-    __baspar_indicator_end_override_aws $AWS_ACCOUNT_ID
+    __baspar_indicator_end_override_aws "$AWS_ACCOUNT_ID - $AWS_PRETTY_ROLE"
   end
 end
 
